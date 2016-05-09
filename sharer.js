d95653a2f1a22764c4d3d2e1f55510df80789d6a
Ahmed Badawy <admin@ahmed-badawy.com>
2016-05-09 20:33:55 +0200

    all

--- sharer.js
+++ sharer.js
@@ -20,29 +20,29 @@
 
 		else if(share_type=='google+') $link= `https://plus.google.com/share?url=${shared_data.prepared_url}&gpsrc=frameless`;
 
-    else if(share_type=="linkedin") $link = "https://www.linkedin.com/shareArticle?mini=true&url="+shared_data.url+"&title="+shared_data.title+"&summary="+shared_data.prepared_text;
+    else if(share_type=="linkedin") $link = `https://www.linkedin.com/shareArticle?mini=true&url=${shared_data.url}&title=${shared_data.title}&summary=${shared_data.prepared_text}`;
 
-    else if(share_type=="pinterest") $link = "https://www.pinterest.com/pin/create/button/?url="+shared_data.prepared_url+"&media="+shared_data.img+"&description="+shared_data.prepared_text;
+    else if(share_type=="pinterest") $link = `https://www.pinterest.com/pin/create/button/?url=${shared_data.prepared_url}&media=${shared_data.img}&description=${shared_data.prepared_text}`;
 
-		else if(share_type=="stumbleupon") $link = "http://www.stumbleupon.com/submit?url="+shared_data.prepared_url+"&title="+shared_data.title;
+		else if(share_type=="stumbleupon") $link = `http://www.stumbleupon.com/submit?url=${shared_data.prepared_url}&title=${shared_data.title}`;
 
-		else if(share_type=="vk") $link = "https://vk.com/share.php?url="+shared_data.prepared_url+"&title="+shared_data.title+"&description="+shared_data.prepared_text+"&image="+shared_data.img+"&noparse=true";
+		else if(share_type=="vk") $link = `https://vk.com/share.php?url=${shared_data.prepared_url}&title=${shared_data.title}&description=${shared_data.prepared_text}&image=${shared_data.img}&noparse=true`;
 
-		else if(share_type=="xing") $link = "https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url="+shared_data.prepared_url;
+		else if(share_type=="xing") $link = `https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=${shared_data.prepared_url}`;
 
-		else if(share_type=="tumblr") $link = "http://www.tumblr.com/share/link?url="+shared_data.prepared_url+"&title="+shared_data.prepared_title+"&description="+shared_data.prepared_text;
- //`http://tumblr.com/widgets/share/tool?canonicalUrl=${url}&url=${url}&posttype=link&title=${title}&caption=${cap}&content=${cont}`;
+		else if(share_type=="tumblr") $link = `http://www.tumblr.com/share/link?url=${shared_data.prepared_url}&title=${shared_data.prepared_title}&description=${shared_data.prepared_text}`;
+    //http://tumblr.com/widgets/share/tool?canonicalUrl=${url}&url=${url}&posttype=link&title=${title}&caption=${cap}&content=${cont}`;
 
 
-		else if(share_type=="reddit-text") $link = "http://www.reddit.com/submit?title="+shared_data.title+"&kind=text&text="+shared_data.text;
+		else if(share_type=="reddit-text") $link = `http://www.reddit.com/submit?title=${shared_data.title}&kind=text&text=${shared_data.text}`;
 
-		else if(share_type=="reddit-link") $link = "http://www.reddit.com/submit?title="+shared_data.title+"kind=link&url="+shared_data.prepared_url;
+		else if(share_type=="reddit-link") $link = `http://www.reddit.com/submit?title=${shared_data.title}+"kind=link&url=${shared_data.prepared_url}`;
 
 		else if(share_type=='buffer') $link = `http://bufferapp.com/add?text=${shared_data.text}&url=${shared_data.prepared_url}`;
 
 		else if(share_type=='digg') $link = `http://digg.com/submit?url=${shared_data.prepared_url}&title=${shared_data.prepared_url}`;
 
-		else if(share_type=='delicious') $link = `https://delicious.com/save?v=5&provider=${shared_data.prepared_url}&noui&jump=close&url=${shared_data.prepared_url}&title=${shared_data.title}`;
+		else if(share_type=='delicious') $link = `https://delicious.com/save?v=5&provider=${shared_data.delicious_provider}&noui&jump=close&url=${shared_data.prepared_url}&title=${shared_data.title}`;
 
 		// else if(share_type=="friendfeed") $link = "http://www.friendfeed.com/share?url="+ shared_data.prepared_url + "&title=" + shared_data.prepared_title;
 
@@ -50,21 +50,19 @@
 
 		// else if(share_type=="blinklist") $link = "http://www.blinklist.com/index.php?Action=Blink/Addblink.php&Url="+ shared_data.prepared_url + "&Title=" + shared_data.prepared_title;
 		//
-		else if(share_type=="plurk") $link = "http://plurk.com/?qualifier=shares&status=" + shared_data.prepared_title + "%20%2D%20" + shared_data.prepared_url;
+		else if(share_type=="plurk") $link = `http://plurk.com/?qualifier=shares&status=${shared_data.prepared_title}%20%2D%20${shared_data.prepared_url}`;
 
+    else if(share_type=="email") $link = `mailto:${shared_data.send_to_email}?subject=${shared_data.title}&body=${shared_data.text}`;
 
 
 
 //---------------------Android----------------------
-		else if(share_type=="email") $link = "mailto:?subject="+shared_data.title+"&body="+shared_data.text;
-
-		else if(share_type=="whatsapp") $link = "whatsapp://send?text="+shared_data.text;
-
-		else if(share_type=="call_phone") $link = "tel:"+shared_data.android_phone_num;
-
-		else if(share_type=="internet_call") $link = "callTo:"+shared_data.android_phone_num;
-
-		else if(share_type=="google_maps") $link = "geo:"+shared_data.android_google_maps_acc;
+		else if(share_type=="whatsapp") $link = `whatsapp://send?text=${shared_data.text}`;
+		else if(share_type=="call_phone") $link = `tel:${shared_data.android_phone_num}`;
+		else if(share_type=="internet_call") $link = `callTo:${shared_data.android_phone_num}`;
+		else if(share_type=="google_maps") $link = `geo:${shared_data.android_google_maps_acc}`;
+//---------------------IOS----------------------
+else if(share_type=="telegram") $link = `tg://msg?text=${shared_data.text}`;
 //--------------------------------------------------
 
       return $link;
